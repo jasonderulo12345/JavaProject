88028db554378202ecaeb253255838bae657748a
Paul Ruben <paulruben186@gmail.com>
2021-06-11 01:46:22 +0800

    Implement comparable to User

--- model/User.java
+++ model/User.java
@@ -1,15 +1,19 @@
 package model;
 
-// saya tukar username ke userId jadi consistent sikit 🐱
-public class User {
+public class User implements Comparable<User> {
     private String userId;
     private String password;
     private String fullname;
 
-    public String getUsername() { return userId; }
-    public void setUsername(String userId) { this.userId = userId; }
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
     public String getFullname() { return fullname; }
     public void setFullname(String fullname) { this.fullname = fullname; }
+
+    @Override
+    public int compareTo(User user) {
+        return userId.compareTo(user.getUserId());
+    }
 }
