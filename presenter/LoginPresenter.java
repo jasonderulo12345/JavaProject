2289b155c92c94ea4623549ce4847515ac9ec20f
Willi W <>
2021-06-11 06:21:32 +0000

    sumthing

--- presenter/LoginPresenter.java
+++ presenter/LoginPresenter.java
@@ -3,6 +3,13 @@ package presenter;
 import event.*;
 
 public class LoginPresenter implements Subscriber {
+    private String username;
+    private String password;
+
+    @Override
+    public void onLoginButtonPressed(String, String) {
+      //TODO Auto-generated method stub
+    }
 
     @Override
     public void handleEvent(Event event) {
--- presenter/MealPresenter.java
+++ presenter/MealPresenter.java
@@ -3,6 +3,12 @@ package presenter;
 import event.*;
 
 public class MealPresenter implements Subscriber {
+    private String name;
+    private String foodGroup;
+    private String date;
+    private String day;
+    private String drink;
+
     @Override
     public void handleEvent(Event event) {
         // TODO Auto-generated method stub
