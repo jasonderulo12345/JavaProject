d1dfb998fa714e8323da2941277aee52cc117f90
Paul Ruben <paulruben186@gmail.com>
2021-06-13 18:04:32 +0800

    Minor fixes

--- event/LoginEvent.java
+++ event/LoginEvent.java
@@ -2,13 +2,19 @@ package event;
 
 public class LoginEvent implements Event {
     private String userId;
+    private String fullname;
 
-    public LoginEvent(String userId) {
+    public LoginEvent(String userId, String fullname) {
         this.userId = userId;
+        this.fullname = fullname;
     }
 
     public String getUserId() {
         return userId;
+    }
+
+    public String getFullname() {
+        return fullname;
     }
 
     public void setUserId(String userId) {
